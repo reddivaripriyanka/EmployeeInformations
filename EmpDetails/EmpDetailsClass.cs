689fdf5425d9af7f99320ec974f32f0053c87553
priyanka.reddivari22 <priyanka.reddivari22@gmail.com>
2019-05-16 11:50:07 +0000

    No changes

---
+++ EmpDetails/EmpDetailsClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+namespace EmpDetails
+{
+    public class EmpDetailsClass : TableEntity
+    {
+        public EmpDetailsClass()
+        {
+        }
+
+        public EmpDetailsClass(int empId, string company)
+        {
+            this.RowKey = empId.ToString();
+            this.PartitionKey = company;       
+        }
+        public EmpDetailsClass(int empId,string company,string EmpName, string ContactNo, string Email, string ProfileImage)
+        {
+            this.RowKey = empId.ToString();
+            this.PartitionKey = company;
+            this.EmpName = EmpName;
+            this.ContactNo = ContactNo;
+            this.Email = Email;
+            this.ProfileImage = ProfileImage;
+        }
+        public string EmpName { get; set; }
+        public string ContactNo { get; set; }
+        public string Email { get; set; }
+        public string ProfileImage { get; set; }
+    }
+}
+
+
